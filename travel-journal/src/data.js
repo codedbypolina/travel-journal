c0cbd295efc662477268d19b4652adc632650a98
Polina Shliakhina <98726255+codedbypolina@users.noreply.github.com>
2023-02-22 17:51:35 -0500

    Added additional files

---
+++ travel-journal/src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "Playa Samara",
+        location: "Costa Rica",
+        googleMapsUrl: "https://goo.gl/maps/oeAmUTCDCFLuLeo58",
+        startDate: "6 Aug, 2022",
+        endDate: "20 Aug, 2022",
+        description: "My firs solo trip! During this trip, I lived with Costa Rican family (tica), learned Spanish for three hours almost everyday, explored the beach and the surroundings, as well as met some interesting people!",
+        imageUrl: "https://i.pinimg.com/originals/6f/3d/0c/6f3d0ce13bf002ae6efc18b50fb910f8.jpg"
+    },
+    {
+        title: "Acadia National Park",
+        location: "Maine",
+        googleMapsUrl: "https://goo.gl/maps/F7EfGgtmwDsATrKp7",
+        startDate: "30 Jun, 2022",
+        endDate: "5 Jul, 2022",
+        description: "A beautiful and relaxing trip to Maine. Explored Acadia National Park, stayed at Bar Harbor (aka Baa-Ha-Ba) for a couple of nights and did a lot of reading and relaxing",
+        imageUrl: "https://i.pinimg.com/originals/6f/3d/0c/6f3d0ce13bf002ae6efc18b50fb910f8.jpg"
+    },
+    {
+        title: "National Bonsai & Penjing Museum",
+        location: "Washington D.C.",
+        googleMapsUrl: "https://goo.gl/maps/FCgNuPsqRoKnV2nw7",
+        startDate: "20 Nov, 2021",
+        endDate: "24 Nov, 2021",
+        description: "An interesting and educational trip to DC. Traveled there on train with my boyfriend. We explored Planet Word museum (It's a language arts museum!!! I was SO excited and had the most wonderful time there), National Bonsai & Penjing Museum (A beautiful museum with some of the most iconic Bonsai in the world!), DC National Mall, Arlington National Cemetery and Old Town Alexandria",
+        imageUrl: "https://i.pinimg.com/originals/30/f7/dc/30f7dcbab2d11e2a7a096a6b338d6301.jpg"
+    }
+]
